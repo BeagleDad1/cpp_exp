ce99f0e00403083c6d9d660181e80a3e23e95af3
CodeWarrior <formatt.hoffman@gmail.com>
2018-10-28 09:33:02 -0700

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <string>
+
+class Logger
+{
+public:
+    Logger() :
+        m_logLevel(LogLevelError) {}
+
+    enum LogLevel
+    {
+        LogLevelError = 0,
+        LogLevelWarn,
+        LogLevelInfo
+    };
+private:
+    LogLevel m_logLevel;
+public:
+    void logError(const std::string& message) const
+    {
+        if(LogLevelError <= m_logLevel)
+        {
+            std::cout << "[ERROR]: " << message << std::endl;
+        }
+    }
+};
 
 int main()
 {
     std::cout << "Hello World!!!" << std::endl;
-    
+
+    Logger ll;
+    ll.logError("Wow");
     return 0;
 }
